14d4d74b45ccac135bed9164bce222d2358d67a1
emelieahlin <emelieahlinandersson@gmail.com>
2016-09-09 12:08:58 +0200

    Kopplat på answers från databasen

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from quiz.models import Quiz
+from django.shortcuts import redirect 
 
 
 # Create your views here.
@@ -31,6 +32,15 @@ def question(request, quiz_number, question_number):
 	"quiz_number": quiz_number,
 	}
 	return render(request, "quiz/question.html", context)
+
+def answer(request, quiz_number, question_number):
+	saved_answers = request.session.get(quiz_number,{})
+	answer = int(request.POST["answer"])
+	saved_answers[question_number] = answer
+	request.session[quiz_number] = saved_answers
+
+	question_number = int(question_number)
+	return redirect("question_page", quiz_number, question_number + 1)
 
 def completed(request, quiz_number):
 	context = {
